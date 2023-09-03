328051e7474e95709e723c8d9816c95445eda4fc
Alexandre Miguel <alexandremiguel.dev@gmail.com>
2023-09-02 22:22:39 -0300

    feat: add print currency

---
+++ scripts/print-currency.js
@@ -0,0 +1,28 @@
+const list = document.querySelector('[data-list]')
+
+function generateCurrencyItems(name, value) {
+	const currencyItems = []
+
+	for (let mult = 1; mult <= 1000; mult *= 10) {
+		const formattedValue = (value * mult).toFixed(2)
+		currencyItems.push(`${mult} ${name}: R$${formattedValue}`)
+	}
+
+	return currencyItems
+}
+
+function printCurrencyList(currencyItems) {
+	const list = document.querySelector('[data-list]')
+	list.innerHTML = ''
+
+	currencyItems.forEach((itemText) => {
+		const item = document.createElement('li')
+		item.textContent = itemText
+		list.appendChild(item)
+	})
+}
+
+export default function printCurrency(name, value) {
+	const currencyItems = generateCurrencyItems(name, value)
+	printCurrencyList(currencyItems)
+}
